77caa4c176ff0d9b01d6dcae657713c1c747c1a8
Peter-DevX <peterasiedugyan0@gmail.com>
2024-11-24 15:08:10 +0000

    changes as at 24/11/2024
    Removed quick links from footer and changes to adjust nav-menu bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,14 @@ navLink.forEach((nav) => {
 });
 
 
-
+function showSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+}
+function closeSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+}
 
 
 
